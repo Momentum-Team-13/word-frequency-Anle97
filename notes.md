60c732b96444429398d859715900d76d9368fa3e
An <an.tle.914@gmail.com>
2022-06-16 11:40:52 -0400

    file is cleaned, work count not created yet

---
+++ notes.md
@@ -0,0 +1,6 @@
+# Steps for this project:
+- Read in the text file
+- Clean the data per steps below
+- Remove the words
+- Make a count of each word
+- Print out a report that shows the number of times each word appears in a file
--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,32 @@
+import string
+
+from bleach import Cleaner
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+words = []
+word_dict = {}
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    print(f'Your file is: {file}')
+    with open(file) as open_file:
+        read_file = open_file.read()
+        # makes text lowercase, removes punctuation and makes string into a list
+        clean_file = read_file.translate(str.maketrans('', '', string.punctuation)).lower().split()
+        # removes stop words
+        cleanest_file = [word for word in clean_file if word not in STOP_WORDS] 
+    
+    print(cleanest_file)
+    print(len(cleanest_file))
+
+
+
+
 
 
 if __name__ == "__main__":
