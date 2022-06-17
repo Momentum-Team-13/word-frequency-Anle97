0f65eb5cbe56cb8aee62a7afa1fb0fd90695c0cb
An <an.tle.914@gmail.com>
2022-06-17 10:41:59 -0400

    Completed project

--- word_frequency.py
+++ word_frequency.py
@@ -8,9 +8,31 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
-words = []
+#empty dictionaries to be populated
 word_dict = {}
+organized_dict = {}
 
+#function to count words/populate dictionaries
+def count_words(text):
+    #counts the number of words and puts into word_dict
+    for word in text:
+        if word_dict.get(word) == None:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    #creates a new dictionary that sorts word_dict
+    organized_list = sorted(word_dict, key=word_dict.get, reverse=True)
+    for key in organized_list:
+        organized_dict[key] = word_dict[key]
+    #prints the values of organized_dict
+    for key, value in organized_dict.items():
+        stars = ""
+        for i in range(value):
+            stars += '*'
+        print(f'{key:>14} | {value} | {stars}')
+
+
+# function that opens file and prints output in python console
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     print(f'Your file is: {file}')
@@ -19,13 +41,9 @@ def print_word_freq(file):
         # makes text lowercase, removes punctuation and makes string into a list
         clean_file = read_file.translate(str.maketrans('', '', string.punctuation)).lower().split()
         # removes stop words
-        cleanest_file = [word for word in clean_file if word not in STOP_WORDS] 
-    
-    print(cleanest_file)
-    print(len(cleanest_file))
-
-
-
+        cleanest_file = [word for word in clean_file if word not in STOP_WORDS]     
+    #calls function on cleaned file to print output
+    count_words(cleanest_file)
 
 
 
